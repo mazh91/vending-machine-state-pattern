c7f1f53ddec194814a0cbc1fe5b36924f207278c
mazh91 <milad.azh@ryerson.ca>
2018-07-17 05:08:02 -0400

    added product class

---
+++ src/Product.java
@@ -0,0 +1,36 @@
+public class Product {
+
+    String productCode;
+    String description;
+    double price;
+
+    public Product(String productCode, String description, double price){
+        this.productCode = productCode;
+        this.description = description;
+        this.price = price;
+    }
+
+    public String getProductCode() {
+        return productCode;
+    }
+
+    public void setProductCode(String productCode) {
+        this.productCode = productCode;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
