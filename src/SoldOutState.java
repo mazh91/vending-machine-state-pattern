dcb1303b21dd17d5a0b6ebe438bfa01dbd596f62
mazh91 <milad.azh@ryerson.ca>
2018-07-17 21:35:38 -0400

    implement sold out state

--- src/SoldOutState.java
+++ src/SoldOutState.java
@@ -1,21 +1,29 @@
 public class SoldOutState implements State {
+
+    private VendingMachine vendingMachine;
+
+    public SoldOutState(VendingMachine vendingMachine){
+        this.vendingMachine = vendingMachine;
+    }
+
     @Override
     public void insertCoin(double amt) {
-
+        System.err.println("Operation not possible");
     }
 
     @Override
     public void inputItemCode(String code) {
-
+        System.err.println("Operation not possible");
     }
 
     @Override
     public void returnCoins() {
-
+        System.err.println("Operation not possible");
     }
 
     @Override
     public void dispenseItem() {
-
+        System.err.println("Item sold out!");
+        vendingMachine.setState(vendingMachine.getCoinsInsertedState());
     }
 }
