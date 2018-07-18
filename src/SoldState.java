d1407077470fd4325a4b7ac7ac37b37134f81c3a
mazh91 <milad.azh@ryerson.ca>
2018-07-17 21:36:13 -0400

    implement sold state

--- src/SoldState.java
+++ src/SoldState.java
@@ -1,21 +1,38 @@
 public class SoldState implements State {
+
+    private VendingMachine vendingMachine;
+
+    public SoldState(VendingMachine vendingMachine){
+        this.vendingMachine = vendingMachine;
+    }
+
     @Override
     public void insertCoin(double amt) {
-
+        System.err.println("Operation not possible");
     }
 
     @Override
     public void inputItemCode(String code) {
-
+        System.err.println("Operation not possible");
     }
 
     @Override
     public void returnCoins() {
-
+        System.err.println("Operation not possible");
     }
 
     @Override
     public void dispenseItem() {
+        int count = vendingMachine.getProduct().getCount();
+        vendingMachine.getProduct().setCount( count-1 );
+        vendingMachine.reduceFunds( vendingMachine.getProduct().getPrice() );
 
+        System.out.println("Dispensing..");
+        System.out.flush();
+
+        if(vendingMachine.getFunds() > 0.0)
+            vendingMachine.setState(vendingMachine.getCoinsInsertedState());
+        else
+            vendingMachine.setState(vendingMachine.getReadyState());
     }
 }
